1cf1e41e1f32c7864ab7a66643765e90614d6098
Maxime Antoine <maxime.antoine@asia.bnpparibas.com>
2015-07-31 16:00:00 +0800

    Working JWT basic auth system

--- Bouchon.API/App_Start/AuthConfig.cs
+++ Bouchon.API/App_Start/AuthConfig.cs
@@ -1,11 +1,11 @@
 ﻿using Bouchon.API.Authentication;
 using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
 using System;
 using System.Configuration;
-using System.Security.Cryptography.X509Certificates;
-using Thinktecture.IdentityServer.AccessTokenValidation;
-using Thinktecture.IdentityServer.Core.Configuration;
 
 namespace Bouchon.API
 {
@@ -13,39 +13,77 @@ namespace Bouchon.API
     {
         public static void Config(IAppBuilder app)
         {
-            //setup identity server
-            app.Map("/identity", idsrvApp =>
-                {
-                    idsrvApp.UseIdentityServer(new IdentityServerOptions
-                        {
-                            SigningCertificate = LoadCertificate(),
-                            Factory = InMemoryFactory.Create(
-                                users: Users.Get(),
-                                clients: Clients.Get(),
-                                scopes: Scopes.Get()
-                            )
-                        });
-                });
+            ////setup identity server
+            //app.Map("/identity", idsrvApp =>
+            //    {
+            //        idsrvApp.UseIdentityServer(new IdentityServerOptions
+            //            {
+            //                SigningCertificate = LoadCertificate(),
+            //                Factory = InMemoryFactory.Create(
+            //                    users: Users.Get(),
+            //                    clients: Clients.Get(),
+            //                    scopes: Scopes.Get()
+            //                )
+            //            });
+            //    });
 
-            //consume tokens emitted by identity server
-            app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions
-                {
-                    Authority = ConfigurationManager.AppSettings["appRootUrl"] + "identity",
-                    RequiredScopes = new string[] { "api" },
-                    AuthenticationMode = AuthenticationMode.Active
-                });
+            ////consume tokens emitted by identity server
+            //app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions
+            //    {
+            //        Authority = ConfigurationManager.AppSettings["appRootUrl"] + "identity",
+            //        RequiredScopes = new string[] { "api" },
+            //        AuthenticationMode = AuthenticationMode.Active
+            //    });
+
+            _ConfigureJwtTokenGeneration(app);
+            _ConfigureJwtTokenConsumption(app);
         }
 
-        private static X509Certificate2 LoadCertificate()
+        //
+        private static void _ConfigureJwtTokenGeneration(IAppBuilder app)
         {
-            var store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
-            store.Open(OpenFlags.ReadOnly);
+            //JWT token generation
+            var OAuthServerOptions = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = false,
+                TokenEndpointPath = new Microsoft.Owin.PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(2),
+                Provider = new CustomOAuthProvider(),
+                AccessTokenFormat = new CustomJwtFormat(ConfigurationManager.AppSettings["appRootUrl"])
+            };
 
-            foreach (var cert in store.Certificates)
-                if (cert.FriendlyName == "Development Certificate")
-                    return cert;
-
-            throw new Exception("Dev SSL certificate not found !");
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
         }
+
+        private static void _ConfigureJwtTokenConsumption(IAppBuilder app)
+        {
+            //JWT token consumption
+            var issuer = ConfigurationManager.AppSettings["appRootUrl"];
+            var audienceId = ConfigurationManager.AppSettings["as:AudienceId"];
+            byte[] audienceSecret = TextEncodings.Base64Url.Decode(ConfigurationManager.AppSettings["as:AudienceSecret"]);
+
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    AllowedAudiences = new[] { audienceId },
+                    IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
+                    {
+                        new SymmetricKeyIssuerSecurityTokenProvider(issuer, audienceSecret)
+                    }
+                });
+        }
+
+        //private static X509Certificate2 LoadCertificate()
+        //{
+        //    var store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
+        //    store.Open(OpenFlags.ReadOnly);
+
+        //    foreach (var cert in store.Certificates)
+        //        if (cert.FriendlyName == "Development Certificate")
+        //            return cert;
+
+        //    throw new Exception("Dev SSL certificate not found !");
+        //}
     }
 }
