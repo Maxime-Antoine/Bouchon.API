b4af9c01e3668d6e674676c30bafea6db4452a8a
Maxime Antoine <maxime.antoine2@gmail.com>
2015-08-11 00:08:43 +0800

    Config sendgrid

--- Bouchon.API/Startup.cs
+++ Bouchon.API/Startup.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Owin;
 using Microsoft.Owin.Cors;
 using Owin;
+using System;
+using System.Configuration;
+using System.Net.Configuration;
+using System.Web.Configuration;
 using System.Web.Http;
 
 [assembly: OwinStartup(typeof(Bouchon.API.Startup))]
@@ -21,6 +25,15 @@ namespace Bouchon.API
 
             app.UseCors(CorsOptions.AllowAll); //Allow CORS for API
             app.UseWebApi(config);
+
+            //configure SMTP
+            var webconfig = WebConfigurationManager.OpenWebConfiguration("~") as System.Configuration.Configuration;
+            var settings = webconfig.GetSectionGroup("system.net/mailSettings") as MailSettingsSectionGroup;
+            settings.Smtp.Network.Host = ConfigurationManager.AppSettings["sg:server"];
+            settings.Smtp.Network.Port = Int32.Parse(ConfigurationManager.AppSettings["sg:port"]);
+            settings.Smtp.Network.UserName = ConfigurationManager.AppSettings["sg:username"];
+            settings.Smtp.Network.Password = ConfigurationManager.AppSettings["sg:password"];
+            webconfig.Save();
         }
     }
 }
